65c732981103b67088a24219162931f9729a656f
Harshikesh <harshibabu604@gmail.com>
2023-10-06 01:56:45 +0530

    test annotation removed from test class and insertStatements added

---
+++ InsertStatements/warehouseInsert.sql
@@ -0,0 +1,18 @@
+/*
+-- Query: select * from fe_user.warehouse
+LIMIT 0, 1000
+
+-- Date: 2023-10-06 01:34
+*/
+INSERT INTO fe_user.warehouse (`ware_id`,`ware_name`,`ware_code`) VALUES (1,'HOWRAH','HWH');
+INSERT INTO fe_user.warehouse (`ware_id`,`ware_name`,`ware_code`) VALUES (2,'RISHRA','RIS');
+INSERT INTO fe_user.warehouse (`ware_id`,`ware_name`,`ware_code`) VALUES (3,'SERAMPORE','SER');
+INSERT INTO fe_user.warehouse (`ware_id`,`ware_name`,`ware_code`) VALUES (4,'DANKUNI','DAN');
+INSERT INTO fe_user.warehouse (`ware_id`,`ware_name`,`ware_code`) VALUES (5,'RAJARHAT','RAJ');
+INSERT INTO fe_user.warehouse (`ware_id`,`ware_name`,`ware_code`) VALUES (6,'SANTRAGACHI','STR');
+INSERT INTO fe_user.warehouse (`ware_id`,`ware_name`,`ware_code`) VALUES (9,'DURGAPUR','DRG');
+INSERT INTO fe_user.warehouse (`ware_id`,`ware_name`,`ware_code`) VALUES (10,'LILUAH','LIL');
+INSERT INTO fe_user.warehouse (`ware_id`,`ware_name`,`ware_code`) VALUES (11,'CHANDANNAGAR','CGR');
+INSERT INTO fe_user.warehouse (`ware_id`,`ware_name`,`ware_code`) VALUES (12,'SODEPUR','SDP');
+INSERT INTO fe_user.warehouse (`ware_id`,`ware_name`,`ware_code`) VALUES (13,'SANKRAIL','SKR');
+INSERT INTO fe_user.warehouse (`ware_id`,`ware_name`,`ware_code`) VALUES (14,'KONNAGAR','KGR');
