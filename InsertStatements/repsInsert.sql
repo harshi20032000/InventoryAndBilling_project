da184dfc602b1d9bacccdee7c39937c01bf482e8
Harshikesh <harshibabu604@gmail.com>
2023-10-06 03:19:06 +0530

    added insert statements for reps

---
+++ InsertStatements/repsInsert.sql
@@ -0,0 +1,15 @@
+/*
+-- Query: select * from fe_user.reps
+LIMIT 0, 1000
+
+-- Date: 2023-10-06 03:13
+*/
+INSERT INTO fe_user.reps (`rep_id`,`rep_location`,`rep_name`) VALUES (1,'Patna','Amit Mishra');
+INSERT INTO fe_user.reps (`rep_id`,`rep_location`,`rep_name`) VALUES (2,'Rajasthan','Rahul Jhanjharia');
+INSERT INTO fe_user.reps (`rep_id`,`rep_location`,`rep_name`) VALUES (3,'Mumbai','Abhijit Saini');
+INSERT INTO fe_user.reps (`rep_id`,`rep_location`,`rep_name`) VALUES (4,'Andhra Pradesh','Kommula Sai Krishna');
+INSERT INTO fe_user.reps (`rep_id`,`rep_location`,`rep_name`) VALUES (5,'Orissa','Abhilash Nayak');
+INSERT INTO fe_user.reps (`rep_id`,`rep_location`,`rep_name`) VALUES (6,'Hyderabad','Ankita');
+INSERT INTO fe_user.reps (`rep_id`,`rep_location`,`rep_name`) VALUES (7,'Bhubhaneshwar','Vivek Singh');
+INSERT INTO fe_user.reps (`rep_id`,`rep_location`,`rep_name`) VALUES (8,'Ahmedabad','Sayan Mondal');
+INSERT INTO fe_user.reps (`rep_id`,`rep_location`,`rep_name`) VALUES (9,'Siwan','Nisha Shaw');
